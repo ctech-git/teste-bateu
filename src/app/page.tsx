b1a00f646c368f7259a5760c705c73b666726201
Tiago Rocha <tiagoarrocha@gmail.com>
2025-03-25 22:34:28 -0300

    Teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <img src="https://dw0cadq7atid9.cloudfront.net/12345678912/1742353979_imposto-ir_teste-online.jpg" />
+        <img src="https://bateubet-documentos.s3.us-east-1.amazonaws.com/12345678912/1742353979_imposto-ir_teste-online.jpg" />
       </main>
     </div>
   );
